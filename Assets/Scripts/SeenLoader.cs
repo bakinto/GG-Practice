459798c4260a7f07aee79f8ad01f880744731a79
bakinto <bakintoo@gmail.com>
2020-03-14 23:29:39 +0100

    Creating Loading Next Scene Script

---
+++ Assets/Scripts/SeenLoader.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class SeenLoader : MonoBehaviour
+{
+    int currentSceneIndex;
+
+    void Start()
+    {
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        Debug.Log(currentSceneIndex);
+        if(currentSceneIndex == 0)
+        {
+            StartCoroutine(Wait8Seconds());
+           
+        }
+    }
+    IEnumerator Wait8Seconds()
+    {
+        
+        yield return new WaitForSeconds(8f);
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+
+}
