ba4aeadf495c45e746364f387934c4f0acb90e16
bakinto <bakintoo@gmail.com>
2020-03-16 19:34:15 +0100

    Working on Spaning

---
+++ Assets/Scripts/AttackerSpawner.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackerSpawner : MonoBehaviour
+{
+    [SerializeField] float min;
+    [SerializeField] float max;
+    bool spawn = true;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("update here !!");
+        while (spawn)
+        {
+            StartCoroutine(spawnCoroutine());
+            
+        }
+    }
+
+    IEnumerator spawnCoroutine()
+    {
+        spoawnAttacker();
+       
+        yield return new WaitForSeconds(UnityEngine.Random.Range(min, max));
+    }
+
+    private void spoawnAttacker()
+    {
+        Debug.Log("Attacker Spawned!!");
+    }
+}
