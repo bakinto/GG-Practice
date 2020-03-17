8d18faf19b72916b439527af1a0e3582bd13e176
bakinto <bakintoo@gmail.com>
2020-03-17 21:57:23 +0100

    Moving zuccini projectile

---
+++ Assets/Scripts/Diffeder.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Diffeder : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject projectile;
+    [SerializeField] int speed = 5;
+    public GameObject gun;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void shootProjectile()
+    {
+        GameObject obj = Instantiate(projectile, gun.transform.position, Quaternion.identity) as GameObject;
+    }
+}
---
+++ Assets/Scripts/ZucciniMove.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZucciniMove : MonoBehaviour
+{
+    [SerializeField] float speed;
+//    public Rigidbody2D rb;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.right * Time.deltaTime * speed);
+    }
+}
