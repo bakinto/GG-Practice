c4a6e57e0e8e7198af722bc0cf63e9cd55fb210f
bakinto <bakintoo@gmail.com>
2020-03-19 21:09:40 +0100

    snap cactus in position

--- Assets/Scripts/DiffedersSpawner.cs
+++ Assets/Scripts/DiffedersSpawner.cs
@@ -5,13 +5,27 @@ using UnityEngine;
 public class DiffedersSpawner : MonoBehaviour
 {
     public GameObject diffender;
+
+    void Update()
+    {
+        getSquareClicked();
+    }
     void OnMouseDown()
     {
-        spawnDefender();
+        spawnDefender(getSquareClicked());
+    }
+    private Vector2 getSquareClicked()
+    {
+        Vector2 mousePos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        Vector2 mouseClickedInWorld = Camera.main.ScreenToWorldPoint(mousePos);
+        float xPos = Mathf.RoundToInt(mouseClickedInWorld.x);
+        float yPos = Mathf.RoundToInt(mouseClickedInWorld.y);
+        Vector2 snapPos = new Vector2(xPos, yPos);
+        return snapPos;
     }
 
-    private void spawnDefender()
+    private void spawnDefender(Vector2 pos)
     {
-        GameObject defender = Instantiate(diffender, transform.position, Quaternion.identity) as GameObject;
+        GameObject defender = Instantiate(diffender, pos, Quaternion.identity) as GameObject;
     }
 }
