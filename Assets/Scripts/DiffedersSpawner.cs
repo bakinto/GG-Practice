f6aad43a2c47c8b64066df614a457d686da723b5
bakinto <bakintoo@gmail.com>
2020-03-19 20:01:13 +0100

    Defender creation on mouse click

---
+++ Assets/Scripts/DiffedersSpawner.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiffedersSpawner : MonoBehaviour
+{
+    public GameObject diffender;
+    void OnMouseDown()
+    {
+        spawnDefender();
+    }
+
+    private void spawnDefender()
+    {
+        GameObject defender = Instantiate(diffender, transform.position, Quaternion.identity) as GameObject;
+    }
+}
