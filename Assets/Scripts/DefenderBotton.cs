574e67c05f096fc5667245dc9722271860eba0e9
bakinto <bakintoo@gmail.com>
2020-03-20 20:38:11 +0100

    Creating Level 1  Loader & botton ui

---
+++ Assets/Scripts/DefenderBotton.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefenderBotton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnMouseDown()
+    {
+        var bottons = FindObjectsOfType<DefenderBotton>();
+        foreach(var botton in bottons)
+        {
+            botton.GetComponent<SpriteRenderer>().color = Color.gray;
+        }
+        GetComponent<SpriteRenderer>().color = new Color(255, 255, 255);
+    }
+}
